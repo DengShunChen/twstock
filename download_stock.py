ed5129376bd8330a59f79c180de9fcecdf92de42
Chen, Deng-Shun <dschen@cwb.gov.tw>
2019-09-26 08:33:48 +0800

    update stockdb

--- download_stock.py
+++ download_stock.py
@@ -4,19 +4,25 @@ from datetime import datetime
 
 today = datetime.now().strftime('%Y-%m-%d')
 st = StockTools(today)
+logfile = '/home/pi/twstocktiger/downloaded'
+print('i am in')
+def update():
+  saved_stock = []
+  with open(logfile,'r') as f:
+    for line in f:
+      saved_stock.append(line.strip())
 
-saved_stock = []
-with open('downloaded','r') as f:
-  for line in f:
-    saved_stock.append(line.strip())
+  for sid in st.sids:
+    if sid in saved_stock:
+      continue
+    else:
+      print(sid)
+      st.save_stock(sid)
+      with open(logfile,'a') as f:
+        f.write(sid+'\n')
 
-f = open('downloaded','a')
-for sid in st.sids:
-  print(sid)
-  if sid in saved_stock:
+for i in range(5):
+  try:
+    update()
+  except:
     continue
-  else:
-    st.save_stock(sid)
-    f.write(sid+'\n')
-
-f.close()
